998b1af290217c00bdc929ecdca8b2a1479e71ae
Ramon Ramirez <39835725+RRamirez1414@users.noreply.github.com>
2021-04-13 17:24:45 -0600

    setup public folder with css file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 //initialization and middleware
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: 'true' }))
 
 app.get("/", (req, res) => {
