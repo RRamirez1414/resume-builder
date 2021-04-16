5dc86b6444e3ffeb8d7ab6877690b10557d93f43
Raven Alvarez <alvarezrvn@example.com>
2021-04-16 15:50:09 -0600

    fixed a bug and added test user code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
 //initialization and middleware
 const app = express()
@@ -10,12 +11,23 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: 'true' }))
 
 //mongoose database connection
-const uri = "mongodb+srv://admin:ILOyL5Wzxs7UbdDk@webdev.2jqbv.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:ILOyL5Wzxs7UbdDk@webdev.2jqbv.mongodb.net/RESUME_DB?retryWrites=true&w=majority";
 try {
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 } catch (error) {
     handleError(error);
 }
+
+//create test users
+// const NewUser = new User({
+//     firstName: "Hecmar",
+//     lastName: "Arreola",
+//     username: "harreolanmsu",
+//     password: "password2"
+// })
+
+// console.log(NewUser);
+// // NewUser.save()
 
 const db = mongoose.connection
 db.on('error', error => console.error(error));
