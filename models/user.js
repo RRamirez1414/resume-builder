c193a0202d80a296a81d26cdfef9a4122e6edff0
Raven Alvarez <alvarezrvn@example.com>
2021-04-16 16:19:26 -0600

    removed hash code

--- models/user.js
+++ models/user.js
@@ -33,38 +33,38 @@ const userSchema = new mongoose.Schema({
 */
 
 //function that hashes the password before being saved to the database collection
-userSchema.pre("save", function (next) {
-    const user = this
+// userSchema.pre("save", function (next) {
+//     const user = this
 
-    if(this.isModified("password") || this.isNew){
-        bcrypt.genSalt(10, function (saltError, salt) {
-            if(saltError) {
-                return next(saltError)
-            } else {
-                bcrypt.hash(user.password, salt, function(hashError, hash) {
-                    if (hashError) {
-                      return next(hashError)
-                    }
+//     if(this.isModified("password") || this.isNew){
+//         bcrypt.genSalt(10, function (saltError, salt) {
+//             if(saltError) {
+//                 return next(saltError)
+//             } else {
+//                 bcrypt.hash(user.password, salt, function(hashError, hash) {
+//                     if (hashError) {
+//                       return next(hashError)
+//                     }
           
-                    user.password = hash
-                    next()
-                })
-            }
-        })
-    } else {
-        return next()
-    }
-})
+//                     user.password = hash
+//                     next()
+//                 })
+//             }
+//         })
+//     } else {
+//         return next()
+//     }
+// })
 
 //method to compare passwords submitted by user in login page to verify user
-userSchema.methods.comparePassword = function(password, callback) {
-    bcrypt.compare(password, this.password, function(error, isMatch) {
-        if(error) {
-            return callback(error)
-        } else {
-            callback(null, isMatch);
-        }
-    })
-}
+// userSchema.methods.comparePassword = function(password, callback) {
+//     bcrypt.compare(password, this.password, function(error, isMatch) {
+//         if(error) {
+//             return callback(error)
+//         } else {
+//             callback(null, isMatch);
+//         }
+//     })
+// }
 
 module.exports = mongoose.model('User', userSchema)
