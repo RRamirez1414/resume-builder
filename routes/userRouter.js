a947380961392e283b3189ac12510f6452982bfe
Raven Alvarez <alvarezrvn@example.com>
2021-05-07 16:58:19 -0600

    added saving new resume, TODO: save edu and exp

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -25,7 +25,7 @@ router.get('/:username', (req, res) => {
 router.get('/:username/new-form', async (req, res) => {
   let states = await States.find({})
   let months = await Months.find({})
-  res.render('user/new-website-form', { states: states, months: months })
+  res.render('user/new-website-form', { states: states, months: months, username: req.params.username})
 })
 
 //host/user/:username/view-list
@@ -81,7 +81,43 @@ router.post('/confirm-edits', (req, res) => {
   res.redirect(`${loggedUser.username}/view-list`)
 })
 
+router.post('/save-resume', async (req, res) => {
+  const userID = await User.findOne({username: req.body.username.trim()});
+  const info = req.body;
+  const resumeInfo = new ResumeInfo({
+    user: userID['_id'],
+    siteTitle: info.siteTitle,
+    address: info.address,
+    phone1Type: info.phone1Type,
+    phone2Type: info.phone2Type,
+    phone1: info.phone1,
+    phone2: info.phone2,
+    link1: info.link1,
+    link2: info.link2,
+    email1: info.email1,
+    email2: info.email2,
+    profSum: info.profSum,
+    skills: ""
+  });
+  info.Skills.forEach((skill, idx, array) => {
+    if(idx === array.length - 1){
+      resumeInfo.skills += skill;
+    } else{
+      resumeInfo.skills += skill + ",";
+    }
+  });
+  console.log(resumeInfo);
+  try {
+    const newResumeInfo = await resumeInfo.save();
+  } catch (error) {
+    console.log("error saving resumeInfo")
+    console.log(error);
+  }
+  res.send("save complete");
+});
+
 router.post('/create-website', (req, res) => {
+  res.send(req.body);
   //RR:
   //change route to create-website, update
   //handle form data from url req body, create and to resumeInfo DB
